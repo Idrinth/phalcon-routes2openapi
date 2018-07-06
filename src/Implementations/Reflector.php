bc35b132fe85859c1d2b2c6784dbdf386e11f30d
Björn Büttner <Idrinth@users.noreply.github.com>
2018-07-06 12:58:16 +0200

    handling incomplete return code annotations better

--- src/Implementations/Reflector.php
+++ src/Implementations/Reflector.php
@@ -34,26 +34,33 @@ class Reflector
     public function __invoke(string $class, string $method):array
     {
         try {
-        if(!isset($this->cache[$class])) {
-            $this->cache[$class]['____class'] = new ReflectionClass($class);
-        }
-        if(!isset($this->cache[$class][$method])) {
-            $this->cache[$class][$method] = $this->getReflect($this->cache[$class]['____class'], $method);
-        }
-        return $this->cache[$class][$method];
+            if(!isset($this->cache[$class])) {
+                $this->cache[$class]['____class'] = new ReflectionClass($class);
+            }
+            if(!isset($this->cache[$class][$method])) {
+                $this->cache[$class][$method] = $this->getReflect($this->cache[$class]['____class'], $method);
+            }
+            return $this->cache[$class][$method];
         } catch(Exception $e) {
             return [];
         }
+    }
+    private function getMimeAndSchemaFromTag($tag)
+    {
+        if(!preg_match('#^[a-z0-9.\-_]+/[a-z0-9.\-_]+($|\s)#', $tag)) {
+            return ['*/*', $tag];
+        }
+        return explode(" ", $tag, 2);
     }
     private function getReflect(ReflectionClass $class, string $method)
     {
         $docBlock = $this->parser->create($class->getMethod($method));
         $data = [];
         foreach ($docBlock->getTags() as $tag) {
-            if(preg_match('/^return-([1-9][0-9]{2})$/', $tag->getName(), $matches)) {
-                $parts = explode(" ", "$tag", 2);
-                $data[$matches[1]] = Merger::arrayMergeRecursiveNoConversion(
-                    $data[$matches[1]]??[],
+            if(preg_match('/^return-([1-9][0-9]{2})$/', $tag->getName(), $code)) {
+                $parts = $this->getMimeAndSchemaFromTag("$tag");
+                $data[$code[1]] = Merger::arrayMergeRecursiveNoConversion(
+                    $data[$code[1]]??[],
                     [
                         "description" => '',
                         "content" => [
