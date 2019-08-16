b928d873dffdbf8298360661940606112416b94c
Björn Büttner <Idrinth@users.noreply.github.com>
2019-08-16 09:43:19 +0200

    adding missing method definitions

--- src/Interfaces/Controller.php
+++ src/Interfaces/Controller.php
@@ -25,4 +25,18 @@ interface Controller
      * @return-200 application/json {"type":"object"}
      */
     public function indexAction():ResponseInterface;
+
+    /**
+     * Generates an overview over routes registered
+     * @return-204 {"type":"string","maxLength":0}
+     * @return ResponseInterface
+     */
+    public function options(): ResponseInterface;
+
+    /**
+     * Generates an overview over routes registered
+     * @return-204 {"type":"string","maxLength":0}
+     * @return ResponseInterface
+     */
+    public function optionsAction();
 }
