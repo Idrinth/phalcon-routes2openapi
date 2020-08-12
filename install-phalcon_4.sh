b257db39f75eeeaecd99a0e420a05e7b62c4721e
Björn Büttner <Idrinth@users.noreply.github.com>
2020-08-13 00:05:29 +0200

    adding zephir extension

--- install-phalcon_4.sh
+++ install-phalcon_4.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
+
 mkdir tmp && \
 mv composer.json tmp/ && \
+
+git clone git://github.com/phalcon/php-zephir-parser.git && \
+cd php-zephir-parser && \
+phpize && \
+./configure && \
+make && \
+make install && \
+echo "extension = zephir_parser.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini &&\
+cd .. && \
+
 composer init --name tmp/tmp && \
 composer config repositories.phalcon-zephir-update '{"type":"vcs","url":"https://github.com/idrinth/phalcon-ci-installer","no-api":true}' && \
 composer require --dev techpivot/phalcon-ci-installer:dev-patch-1 && \
 vendor/bin/install-phalcon.sh 4.0.x && \
+
 rm composer.lock && \
 rm composer.json && \
 mv tmp/composer.json ./ && \
-rmdir tmp
+rmdir tmp && \
+rm -r php-zephir-parser
